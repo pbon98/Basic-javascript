c0a7ce0798ed40795ba5ed1732bd38bcb94f804f
pbon98 <pbaughan15@gmail.com>
2023-02-18 15:21:08 -0500

    step 38 basic.js free-code-camp

---
+++ step38.js
@@ -0,0 +1,8 @@
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+const wordBlanks = " I got bit by a " + myAdjective + " " + myNoun + " so I " + myVerb + " away very " + myAdverb + "." ; // Change this line
+// Only change code above this line
