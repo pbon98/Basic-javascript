b06d6690ec7ab4955c0025048198d1e173ab87e3
pbon98 <pbaughan15@gmail.com>
2023-02-24 20:14:08 -0500

    step 36 basic.js free-code-camp

--- step36.js
+++ step36.js
@@ -3,3 +3,4 @@ const lastName = "Lovelace";
 
 // Only change code below this line
 const lastLetterOfLastName = lastName[lastName.length - 1]; // Change this line
+console.log(lastLetterOfLastName)
