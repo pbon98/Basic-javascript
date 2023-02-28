a3d6e0fd6c7354d6d4b1333ef52db1bf67a94d56
pbon98 <pbaughan15@gmail.com>
2023-02-27 20:34:20 -0500

    step 72 basic.js free-code-camp

--- step72.js
+++ step72.js
@@ -2,11 +2,11 @@ function testElseIf(val) {
     if (val > 10) {
       return "Greater than 10";
     }
-  
+
      else if (val < 5) {
       return "Smaller than 5";
     } else {
-      
+      console.log(val < 5)
     }
   
     return "Between 5 and 10";
