80d0b8bac8a0d5b985b1aaa1bd93e3f5c06a1819
pbon98 <pbaughan15@gmail.com>
2023-02-27 20:50:29 -0500

    step 84 basic.js free-code-camp

--- step84.js
+++ step84.js
@@ -4,7 +4,7 @@ const testObj = {
     "shirt": "jersey",
     "shoes": "cleats"
   };
-  
+  console.log(testObj)
   // Only change code below this line
   const hatValue = testObj.hat;      // Change this line
   const shirtValue = testObj.shirt;    // Change this line
