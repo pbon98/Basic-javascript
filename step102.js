6684c18ac9f682e4df83f152b8e64d8d9474eb82
pbon98 <pbaughan15@gmail.com>
2023-02-27 21:09:00 -0500

    step 102 basic.js free-code-camp

--- step102.js
+++ step102.js
@@ -1,5 +1,6 @@
 function multiplyAll(arr) {
     let product = 1;
+    console.log(arr)
     // Only change code below this line
   for (var i = 0; i < arr.length; i++){
     for (var j = 0; j < arr[i].length; j++) {
@@ -11,4 +12,4 @@ function multiplyAll(arr) {
   }
   
   multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);
-  console.log(multiplyAll)
+  
