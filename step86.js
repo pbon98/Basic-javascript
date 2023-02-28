84c6ec57b22d80b3b4bc4ea79c8156a97f06d79f
pbon98 <pbaughan15@gmail.com>
2023-02-27 20:51:45 -0500

    step 86 basic.js free-code-camp

--- step86.js
+++ step86.js
@@ -4,7 +4,7 @@ const testObj = {
     16: "Montana",
     19: "Unitas"
   };
-  
+  console.log(testObj)
   // Only change code below this line
   const playerNumber = 16;  // Change this line
   const player = testObj[playerNumber];   // Change this line
