993d45166fd22a0c2e43012a8efd6798be672fcd
pbon98 <pbaughan15@gmail.com>
2023-02-28 18:01:48 -0500

    step 105 basic.js free-code-camp

---
+++ step105.js
@@ -0,0 +1,10 @@
+function randomFraction() {
+
+    // Only change code below this line
+  let rand = Math.random()
+  console.log(rand)
+  return rand;
+    return 0;
+  
+    // Only change code above this line
+  }
