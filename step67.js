8e4bc486b6a7ac59673ee3958a012c00ffbad089
pbon98 <pbaughan15@gmail.com>
2023-02-27 20:28:35 -0500

    step 67 basic.js free-code-camp

--- step67.js
+++ step67.js
@@ -2,12 +2,10 @@ function testLessThan(val) {
     if (val < 25) {  // Change this line
       return "Under 25";
     }
-  
     if (val < 55) {  // Change this line
       return "Under 55";
     }
-  
     return "55 or Over";
   }
-  
+  console.log (testLessThan)
   testLessThan(10);
