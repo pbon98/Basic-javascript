6dd8f9475195b50952c1cc31a570e2f92ad6d203
pbon98 <pbaughan15@gmail.com>
2023-02-27 20:20:42 -0500

    step 65 basic.js free-code-camp

--- step65.js
+++ step65.js
@@ -2,11 +2,11 @@ function testGreaterThan(val) {
     if (val > 100) {  // Change this line
       return "Over 100";
     }
-  
+    console.log(val > 100)
     if (val > 10) {  // Change this line
       return "Over 10";
     }
-  
+    console.log(val > 10)
     return "10 or Under";
   }
   
